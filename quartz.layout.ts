49066d2ad880c069744d13037eca07707aaf696d
Zach Clare <tclare50@gmail.com>
2025-07-14 22:15:33 +0100

    Quartz sync: Jul 14, 2025, 10:15 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+	globalGraph: {
+	    enableRadial: false
+        }
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
